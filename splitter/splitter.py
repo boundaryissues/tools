04b1a8880723c3ecc1728643366d3d259b534668
Richard Welty <rwelty@averillpark.net>
2014-09-30 15:22:51 -0400

    start of gdal based version of splitter tool

---
+++ splitter/splitter.py
@@ -0,0 +1,26 @@
+''' splitter
+
+This program performs two functions: it splits an ogr format file of
+boundaries into subfiles based on boundary types, and it also converts
+attributes into osm equivalents on the fly. how the split is done and
+what tags are converted are controlled by an external configuration file.
+
+'''
+
+import optparse
+
+import gdal
+from gdalconst import *
+
+import logging as log
+log.basicConfig( level=log.DEBUG, format="%(message)s")
+
+usage = "usage: %prog SRCFILE"
+parser = optparse.OptionParser(usage=usage)
+
+(options, args) = parser.parse_args()
+
+sourceFile = args[0]
+sourceDataSet = gdal.Open( sourceFile, GA_ReadOnly)
+if sourceDataSet is None:
+    exit
