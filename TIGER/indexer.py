e68558c83906e05e01d2545c4679e08dd28ea7aa
Richard Welty <rwelty@averillpark.net>
2014-12-11 10:34:52 -0500

    many improvements, almost right

--- TIGER/indexer.py
+++ TIGER/indexer.py
@@ -4,6 +4,20 @@ import json
 
 #decoder = json.JSONDecoder()
 
+tiger_version = '2014'
+
+
+url_prefix = 'https://github.com/boundaryissues/TIGER'
+url_middle = '/tree/master/'
+
+full_url_prefix = url_prefix + tiger_version + url_middle
+
+embed_prefix = "https://embed.github.com/view/geojson/boundaryissues/TIGER"
+embed_middle = '/master/'
+
+def get_type (string):
+    return string.partition("/")[2].partition("/")[0]
+
 # traverse repository, find METADATA.json files
 
 index = []
@@ -13,18 +27,18 @@ for root, dirs, files in os.walk( "."):
         if name == 'METADATA.json':
 # filter metadata file?
 # load metadata file
-#            print( os.path.join( root, name))
-#            print( root, name)
+
             inputFile = open( os.path.join( root, name), "rb")
             result = json.load( inputFile)
 
 #     build index data structure
 
-            if not 'State' in result['Location'] :
-                continue
-
             files = {}
             entry = {}
+            if 'State' in result['Location'] :
+                state = result['Location']['State']
+            else :
+                state = None
             candidate_files = os.listdir( root)
             for f in candidate_files:
                 if f.endswith( ".geojson") or f.endswith( ".zip"):
@@ -39,18 +53,23 @@ for root, dirs, files in os.walk( "."):
                     else :
                         files[fileroot] = [typ]
                     if typ == 'geojson' :
-                        embed = "https://embed.github.com/view/geojson"
-                        embed = embed + "/openpolygons/TIGER2014/master/"
-                        type2 = root.partition("/")
-                        type3 = type2[2].partition( "/")
-                        embed = embed + type3[0] + "/"
+                        embed = embed_prefix + tiger_version + embed_middle
+                        embed = embed + get_type( root) + "/"
+                        if not state is None :
+                            embed = embed + state + "/"
                         embed = embed + f
                         files[fileroot].append( {'embed' : embed})
 
-            entry['prefix'] = "prefix for fetching from git goes here"
+            url = full_url_prefix + get_type( root) + "/"
+            if not state is None :
+                url = url + state + "/"
+            entry['prefix'] = url
             entry['files'] = files
             entry['metadata'] = name
-            index_entry = {'state' : result['Location']['State']}
+            if state is None :
+                index_entry = {'country' : 'US'}
+            else :
+                index_entry = {'country' : 'US', 'state' : result['Location']['State']}
             entry['indexes'] = [ index_entry]
 
             index.append( entry)
